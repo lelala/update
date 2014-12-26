88abde6ecc0d1b8f13e6c053863eb90def0b0b09
lelala <lelala@ischool.com.tw>
2014-12-26 16:13:43 +0800

    修改寫法

--- server.js
+++ server.js
@@ -5,7 +5,25 @@ var config = require('./config.json');
 
 config.targets.forEach(function (target) {
     express.get('/' + target.name, function (req, res) {
-        procstreams(('git pull', [], { cwd: target.path })).data(function (err, stdout, stderr) {
+        var log = '`';
+        var spawn = require('child_process').spawn;
+        var git = spawn('git', ['pull'], { cwd: target.path });
+        git.stdout.on('data', function (data) {
+            log += (log == '`'?'':'\n`') + data;
+            console.log(data);
+            if (data == "Password:" && target.git && target.git.password)
+                git.stdin.write(target.git.password);
+            git.stdin.write('\n');
+        });
+        git.on('exit', function (code) {
+            var data = 'git process exited with code ' + code;
+            log += (log == '`'?'':'\n`') + data;
+            console.log(data);
+            res.end(log);
+        });
+        return;
+        procstreams('git pull', null, { cwd: target.path })
+        .data(function (err, stdout, stderr) {
             console.log(stdout); // prints number of lines in the file lines.txt
             res.end(stdout);
         });
