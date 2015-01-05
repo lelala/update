a42e2d6bcd75bd8afa4323786848dcb442bc558e
lelala <lelala@ischool.com.tw>
2015-01-05 11:58:29 +0800

    v1.0.0

--- server.js
+++ server.js
@@ -3,58 +3,12 @@ var http = require('http');
 var express = require('express')();
 var config = require('./config.json');
 
-express.get('/', function (req, res) {
-    var log = '`';
-    var spawn = require('child_process').spawn;
-    var cat = spawn('cat', ['server.js'], { cwd: '/www/update' });
-    cat.stdout.on('data', function (data) {
-        log += (log == '`'?'':'\n') + data;
-        console.log(data);
-        //if (data == "Password:" && target.git && target.git.password)
-        //    git.stdin.write(target.git.password);
-        //git.stdin.write('\n>');
-    });
-    cat.on('exit', function (code) {
-        var data = 'cat process exited with code ' + code;
-        log += (log == '`'?'':'\n') + data;
-        console.log(data);
-        res.end(log);
-    });
-    return;
-});
-
 config.targets.forEach(function (target) {
     express.get('/' + target.name, function (req, res) {
-        //var log = '`';
-        //var spawn = require('child_process').spawn;
-        //var git = spawn('git', ['pull'], { cwd: target.path });
-        //process.stdin.on('readable', function () {
-        //    process.stdin.resume();
-        //    var data = process.stdin.read();
-        //    log += (log == '`'?'':'\n>`') + data;
-        //    console.log(data);
-        //    if (data == "Password:") {
-        //        if (target.git && target.git.password)
-        //            process.stdout.write(target.git.password);
-        //        process.stdout.write('\n>');
-        //    }
-        //});
-        
-        //git.stdout.on('data', function (data) {
-        //    log += (log == '`'?'':'\n>`') + data;
-        //    console.log(data);
-        //});
-        //git.on('exit', function (code) {
-        //    var data = 'git process exited with code ' + code;
-        //    log += (log == '`'?'':'\n>`') + data;
-        //    console.log(data);
-        //    res.end(log);
-        //});
-        //return;
         var log = 'Update ' + target.name + ":";
         var opt = { cwd: target.path };
-        log += "\n\tat: " + new Date().toLocaleString();
-        log += "\n\toptions: " + JSON.stringify(opt) + "\n";
+        log += "\n    at: " + new Date().toLocaleString();
+        log += "\n    options: " + JSON.stringify(opt) + "\n";
         var keeplocal = [].concat(target.keeplocal || []);
         
         var cmdstream = null;
@@ -120,5 +74,3 @@ config.targets.forEach(function (target) {
 http.createServer(express).listen(config.port, function () {
     console.log('Express server listening on port ' + config.port);
 });
-//cd / www / health
-//git pull
